24c8561eafbf0a1ebebb955eaeef220d5cc94214
kohei.sato <night.god.moon.garsu@gmail.com>
2023-08-01 00:14:10 +0900

    add directive auth

--- backend/graph/directive.go
+++ backend/graph/directive.go
@@ -2,9 +2,11 @@ package graph
 
 import (
 	"context"
+	"errors"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/Kohei-Sato-1221/SugarGraphQL/backend/generated"
+	"github.com/Kohei-Sato-1221/SugarGraphQL/backend/middlewares/auth"
 )
 
 var Directive generated.DirectiveRoot = generated.DirectiveRoot{
@@ -13,8 +15,8 @@ var Directive generated.DirectiveRoot = generated.DirectiveRoot{
 
 func IsAuthenticated(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
 	// TODO
-	// if _, ok := auth.GetUserName(ctx); !ok {
-	// 	return nil, errors.New("not authenticated")
-	// }
+	if _, ok := auth.GetUserName(ctx); !ok {
+		return nil, errors.New("not authenticated")
+	}
 	return next(ctx)
 }
